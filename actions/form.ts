c9b2230dbf8f410d525ed88a2e3c64975663e176
meet-tola <wasiusikiru7@gmail.com>
2024-12-17 12:15:06 +0100

    fixing

--- actions/form.ts
+++ actions/form.ts
@@ -317,7 +317,7 @@ export async function SubmitForm(
     );
 
     sendEmail(
-      "Talktrack <support@talktrack.com>",
+      "Talktrack <wasiusikiru7@gmail.com>",
       business.email,
       `New Appointment Submission: ${formTitle}`,
       emailContent
